9ac6734ee2e9c2939bcccd3ea1c6561c5b8fb9e0
wrblr <wrbell@uchicago.edu>
2024-10-26 07:47:22 +0000

    penultimate version

--- app.rb
+++ app.rb
@@ -2,8 +2,45 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:homepage)
+end
+
+get("/square/new") do
+
+erb(:square)
+end
+
+get("/square/results") do
+
+erb(:square_results)
+end
+
+get("/square_root/new") do
+
+erb(:square_root)
+end
+
+get("/square_root/results") do
+
+erb(:square_root_results)
+end
+
+get("/payment/new") do
+
+erb(:payment)
+end
+
+get("/payment/results") do
+
+erb(:payment_results)
+end
+
+get("/random/new") do
+
+erb(:random)
+end
+
+get("/random/results") do
+
+erb(:random_results)
 end
